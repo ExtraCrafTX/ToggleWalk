921ad386c64fd6937bbac2cf5fe552cf04d76b39
Kaamil Jasani <kaamilj@yahoo.com>
2019-05-30 16:02:48 +0100

    Simplified code, fixed error, v1.0

--- src/main/java/com/extracraftx/minecraft/togglewalk/ToggleWalk.java
+++ src/main/java/com/extracraftx/minecraft/togglewalk/ToggleWalk.java
@@ -1,49 +1,32 @@
 package com.extracraftx.minecraft.togglewalk;
 
-import java.util.HashMap;
-
 import com.extracraftx.minecraft.togglewalk.interfaces.ToggleableKeyBinding;
 
-import net.fabricmc.api.ModInitializer;
+import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.fabric.api.event.client.ClientTickCallback;
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.options.KeyBinding;
 
-public class ToggleWalk implements ModInitializer {
-
-    boolean walking = false;
-    boolean sneaking = false;
+public class ToggleWalk implements ClientModInitializer {
 
     KeyBinding[] bindings;
 
-    HashMap<KeyBinding, Boolean> toggles = new HashMap<>();
-
     @Override
-    public void onInitialize() {
-        System.out.println("OnInitClient");
+    public void onInitializeClient() {
         ClientTickCallback.EVENT.register((mc)->{
             onTick(mc);
         });
     }
 
     public void onTick(MinecraftClient mc){
-        // System.out.println("OnTick");
         if(bindings == null){
-            System.out.println("BindingsArrayUpdated");
             bindings = new KeyBinding[]{mc.options.keyForward, mc.options.keySneak};
         }
         for(KeyBinding kb : bindings){
-            if(!toggles.containsKey(kb)){
-                System.out.println("KeyBind " + kb.getLocalizedName() + " added to map");
-                toggles.put(kb, false);
-            }
             if(kb.wasPressed()){
-                System.out.println("KeyBind " + kb.getName() + " was pressed/toggled " + toggles.get(kb));
-                toggles.put(kb, !toggles.get(kb));
                 ToggleableKeyBinding tkb = (ToggleableKeyBinding)kb;
-                tkb.setToggled(toggles.get(kb));
+                tkb.toggle();
             }
-            // System.out.println(kb.getLocalizedName() + " isPressed " + kb.isPressed() + " in " + mc.getClass().getName());
         }
     }
 
--- src/main/java/com/extracraftx/minecraft/togglewalk/mixin/KeyBindingMixin.java
+++ src/main/java/com/extracraftx/minecraft/togglewalk/mixin/KeyBindingMixin.java
@@ -3,15 +3,14 @@ package com.extracraftx.minecraft.togglewalk.mixin;
 import com.extracraftx.minecraft.togglewalk.interfaces.ToggleableKeyBinding;
 
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 import net.minecraft.client.options.KeyBinding;
 
 @Mixin(KeyBinding.class)
 public abstract class KeyBindingMixin implements ToggleableKeyBinding{
-
-    @Shadow
-    private boolean pressed;
     
     private boolean toggled = false;
 
@@ -26,9 +25,11 @@ public abstract class KeyBindingMixin implements ToggleableKeyBinding{
         toggled = value;
     }
     
-    public boolean isPressed() {
-        System.out.println("isPressed " + (pressed || toggled));
-        return pressed || toggled;
+    @Inject(method = "isPressed", at = @At("HEAD"), cancellable = true)
+    public void onIsPressed(CallbackInfoReturnable<Boolean> info) {
+        if(toggled){
+            info.setReturnValue(true);
+        }
     }
 
 }
